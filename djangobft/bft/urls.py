21a1ae5ace14696e7f24ce4004327abde46df7ff
Jay McEntire <jay.mcentire@gmail.com>
2023-06-14 12:32:04 -0600

    temp changing url so that old bft works for 7 days

--- djangobft/bft/urls.py
+++ djangobft/bft/urls.py
@@ -13,21 +13,38 @@ urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("acs/", auth.acs, name="acs"),
     path("denied/", auth.denied, name="denied"),
+    # re_path(
+    #     r"^files/(?P<submission_slug>[%s]{%s})/$" % (app_settings.RANDOMSLUG_CHARS, app_settings.RANDOMSLUG_CHAR_NO),
+    #     views.list_files,
+    #     name="files",
+    # ),
     re_path(
-        r"^files/(?P<submission_slug>[%s]{%s})/$" % (app_settings.RANDOMSLUG_CHARS, app_settings.RANDOMSLUG_CHAR_NO),
+        r"^file-list/(?P<submission_slug>[%s]{%s})/$"
+        % (app_settings.RANDOMSLUG_CHARS, app_settings.RANDOMSLUG_CHAR_NO),
         views.list_files,
         name="files",
     ),
     path("process/", views.process_submission, name="process"),
     path("upload/", views.upload, name="upload"),
     path("bftvars.js/", views.render_vars),
+    # re_path(
+    #     r"^(?P<file_slug>[%s]{%s})/$" % (app_settings.RANDOMSLUG_CHARS, app_settings.RANDOMSLUG_CHAR_NO),
+    #     views.get_file,
+    #     name="file",
+    # ),
+    # re_path(
+    #     r"^(?P<file_slug>[%s]{%s})/(?P<file_name>.*)$"
+    #     % (app_settings.RANDOMSLUG_CHARS, app_settings.RANDOMSLUG_CHAR_NO),
+    #     views.get_file,
+    #     name="file",
+    # ),
     re_path(
-        r"^(?P<file_slug>[%s]{%s})/$" % (app_settings.RANDOMSLUG_CHARS, app_settings.RANDOMSLUG_CHAR_NO),
+        r"^file/(?P<file_slug>[%s]{%s})/$" % (app_settings.RANDOMSLUG_CHARS, app_settings.RANDOMSLUG_CHAR_NO),
         views.get_file,
         name="file",
     ),
     re_path(
-        r"^(?P<file_slug>[%s]{%s})/(?P<file_name>.*)$"
+        r"^file/(?P<file_slug>[%s]{%s})/(?P<file_name>.*)$"
         % (app_settings.RANDOMSLUG_CHARS, app_settings.RANDOMSLUG_CHAR_NO),
         views.get_file,
         name="file",
